9ae79775501a4a3c049ca86c65022ece59f82e7f
yusei53 <yusei530922@gmail.com>
2024-08-02 18:10:48 +0900

    initialize kotlin with spring boot

---
+++ src/main/kotlin/com/example/book_manager_serverside/BookManagerServersideApplication.kt
@@ -0,0 +1,11 @@
+package com.example.book_manager_serverside
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManagerServersideApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookManagerServersideApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/book_manager_serverside/BookManagerServersideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.book_manager_serverside
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagerServersideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
